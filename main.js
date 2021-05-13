1f641c07345061aa7666fa32d07b9b5691047834
aniaa19987 <82997712+aniaa19987@users.noreply.github.com>
2021-05-13 10:48:40 +0200

    dodanie warstw
    dodanie województw, mapy topograficznej, sozologicznej, itd.

--- main.js
+++ main.js
@@ -8,4 +8,51 @@ $(document).ready(function () {
 
   var lyrOSM = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png");
   mymap.addLayer(lyrOSM);
+
+  var lyrPRG = L.tileLayer.wms(
+    "http://localhost:8080/geoserver/projekt_prge/wms",
+    {
+      layers: "projekt_prge:Wojewodztwa",
+      format: "image/png",
+      transparent: "true",
+      version: "1.1.1",
+    }
+  );
+
+  var lyrGoogleHyb = L.tileLayer(
+    "https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}"
+  );
+  var lyrSozo = L.tileLayer.wms(
+    "http://mapy.geoportal.gov.pl/wss/service/img/guest/SOZO/MapServer/WMSServer",
+    {
+      layers: "Raster",
+      format: "image/png",
+      transparent: "true",
+      version: "1.1.1",
+    }
+  );
+  var lyrTopo = L.tileLayer.wms(
+    "https://mapy.geoportal.gov.pl/wss/service/img/guest/TOPO/MapServer/WMSServer",
+    {
+      layers: "Raster",
+      format: "image/png",
+      transparent: "true",
+      version: "1.1.1",
+    }
+  );
+
+  var baseMaps = {
+    Openstreetmap: lyrOSM,
+    Województwa: lyrPRG,
+    "Mapa Topograficzna": lyrTopo,
+    "Mapa Sozologiczna": lyrSozo,
+    "Google Hybrid": lyrGoogleHyb,
+  };
+
+  //polecenie dodania ikonki
+  L.control.layers(baseMaps).addTo(mymap);
+
+  L.control
+    .scale({ position: "bottomleft", imperial: false, maxWidth: 200 })
+    .addTo(mymap);
 });
